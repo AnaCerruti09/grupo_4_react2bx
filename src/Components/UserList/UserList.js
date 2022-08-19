327fca0c6bb881569f330ae552af6c3554bf6fa8
nmavric95 <mavricnadia@gmail.com>
2022-08-18 21:46:55 -0300

    agregando listado de usuarios en funionamiento con paginado

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,7 +1,108 @@
 import React from "react"
+import { useState, useEffect } from "react"
 
 function UserList(){
-    return(<div className="UserList">User List</div>)
+    const [users, setUsers] = useState([])
+    const [totalUsers, setTotalUsers] = useState(0)
+    const [range, setRange] = useState([]);
+    const [page, setPage] = useState(1)
+    const [slice, setSlice] = useState([]);
+
+    const api = "http://localhost:3030/api/Users"
+
+    useEffect(()=>{
+        fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            setUsers(data.data)
+            setTotalUsers(data.meta.total)
+        })
+        .catch(error => console.log(error))
+    }, [])
+
+
+
+    const calculateRange = (totalUsers) => {
+        const rowsPerPage = 5
+        const range = [];
+        const num = Math.ceil(totalUsers / rowsPerPage);
+        for (let i = 1; i <= num; i++) {
+          range.push(i);
+        }
+        return range;
+    }
+
+    const sliceData = (users, page) => {
+        const rowsPerPage = 5
+        return users.slice((page - 1) * rowsPerPage, page * rowsPerPage);
+      }
+
+
+
+    useEffect(() => {
+        const range = calculateRange(totalUsers);
+        setRange([...range]);
+    
+        const slice = sliceData(users, page);
+        setSlice([...slice]);
+
+      }, [totalUsers, users, setRange, page, setSlice]);
+    
+    
+    useEffect(() => {
+          if (slice.length < 1 && page !== 1) {
+            setPage(page - 1);
+          }
+    }, [slice, page, setPage])
+    
+
+
+
+
+    return(
+    <React.Fragment>
+        <div className="ContainerList">
+            <div className="TitleListContainer"><h2 className="ListTitle">User List</h2></div>
+            <div className="ListContent">
+                
+            <table className="Table">
+        <thead className="tableRowHeader">
+          <tr>
+            <th className="tableHeader">Nombre</th>
+            <th className="tableHeader">Apellido</th>
+            <th className="tableHeader">Mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {slice.map((el) => (
+            <tr className="tableRowItems" key={el.id}>
+              <td className="tableCell">{el.name}</td>
+              <td className="tableCell">{el.last_name}</td>
+              <td className="tableCell">{el.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table> 
+
+      <div className="tableFooter">
+      {range.map((el, index) => (
+        <button
+          key={index}
+        //   className={`${styles.button} ${
+        //     page === el ? styles.activeButton : styles.inactiveButton
+        //   }`}
+          onClick={() => setPage(el)}
+        >
+          {el}
+        </button>
+      ))}
+    </div>          
+                
+
+            </div>
+        </div>
+    </React.Fragment>
+    )
 }
 
 export default UserList
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,7 +1,26 @@
 import React from "react"
 
+import UserSumUp from "../UserSumUp/UserSumUp"
+import UserList from "../UserList/UserList"
+import SearchUser from "../SearchUser/SearchUser"
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
+
 function Users(){
-    return(<div className="Users">Users</div>)
+    return(
+    <div className="Users">
+        <Header />
+            <br/>
+            <div className="MainContent">
+                <SearchUser />
+                 <br />
+                <UserSumUp />
+                 <br />
+                <UserList />
+            </div>
+        <Footer />
+    </div>)
 }
 
 export default Users
